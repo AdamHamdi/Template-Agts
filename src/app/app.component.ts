27ffda225d44a37d0747bb66a5e0104d8c104ba2
AdamHamdi <prodev.webdesign@gmail.com>
2021-11-11 21:50:35 +0100

    dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+export interface Section {
+  name: string;
+  updated: Date;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showFiller = false;
+  sideNavOpen:boolean=true
+  
+  folders: Section[] = [
+    {
+      name: 'Photos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Recipes',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Work',
+      updated: new Date('1/28/16'),
+    },
+  ];
+  notes: Section[] = [
+    {
+      name: 'Vacation Itinerary',
+      updated: new Date('2/20/16'),
+    },
+    {
+      name: 'Kitchen Remodel',
+      updated: new Date('1/18/16'),
+    },
+  ];
 }
+
